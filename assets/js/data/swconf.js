cab871e26fb2b7ae520ff640e21191ed6223d641
enesuio <enesuio@users.noreply.github.com>
2024-12-29 15:52:56 +0000

    deploy: 0ccf7c67ab1d107935d0868665690e1e8d79c3a3

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,30 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1735487348',resources: [
-      '/assets/css/jekyll-theme-chirpy.css',
-      '/',
-      
-        '/categories/',
-      
-        '/tags/',
-      
-        '/archives/',
-      
-        '/about/',
-      
-
-      
-      
-    ],
-
-    interceptor: {paths: [
-        
-      ],urlPrefixes: [
-        
-      ]
-    },
-
-    purge: false
+    purge: true
   
 };
 
