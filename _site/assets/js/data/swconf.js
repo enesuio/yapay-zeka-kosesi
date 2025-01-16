9f87d76a0da09a68c6fa5fdd32b40fc15ec986ab
Enes Camur <yaebsc@gmail.com>
2025-01-15 20:54:20 -0500

    Edit _config.yml to change baseurl

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,30 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1735485537',resources: [
-      '/assets/css/jekyll-theme-chirpy.css',
-      '/',
-      
-        '/categories/',
-      
-        '/tags/',
-      
-        '/archives/',
-      
-        '/about/',
-      
-
-      
-      
-    ],
-
-    interceptor: {paths: [
-        
-      ],urlPrefixes: [
-        
-      ]
-    },
-
-    purge: false
+    purge: true
   
 };
 
